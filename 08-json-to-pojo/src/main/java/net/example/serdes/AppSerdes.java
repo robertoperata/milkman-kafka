2e65fe467c62f920b8eeeed0e1d583e7dd7931f5
Roberto Perata <robypriz@gmail.com>
2021-04-30 07:20:23 +0200

    avro serialization deserialization

---
+++ 08-json-to-pojo/src/main/java/net/example/serdes/AppSerdes.java
@@ -0,0 +1,46 @@
+package net.example.serdes;
+
+import com.fasterxml.jackson.databind.deser.std.JsonNodeDeserializer;
+import net.example.types.DeliveryAddress;
+import net.example.types.PosInvoice;
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AppSerdes extends Serdes {
+
+
+    static final class PosInvoiceSerde extends WrapperSerde<PosInvoice> {
+        PosInvoiceSerde() {
+            super(new JsonSerializer<>(), new JsonDeserializer<>());
+        }
+    }
+
+    static Serde<PosInvoice> PosInvoice() {
+        PosInvoiceSerde serde = new PosInvoiceSerde();
+
+        Map<String, Object> serdeConfigs = new HashMap<>();
+        serdeConfigs.put(JsonDeserializer.VALUE_CLASS_NAME_CONFIG, PosInvoice.class);
+        serde.configure(serdeConfigs, false);
+
+        return serde;
+    }
+    static final class DeliveryAddressSerde extends WrapperSerde<DeliveryAddress> {
+        DeliveryAddressSerde() {
+            super(new JsonSerializer<>(), new JsonDeserializer<>());
+        }
+    }
+
+    static Serde<DeliveryAddress> DeliveryAddress() {
+        DeliveryAddressSerde serde = new DeliveryAddressSerde();
+
+        Map<String, Object> serdeConfigs = new HashMap<>();
+        serdeConfigs.put(JsonDeserializer.VALUE_CLASS_NAME_CONFIG, DeliveryAddress.class);
+        serde.configure(serdeConfigs, false);
+
+        return serde;
+    }
+
+}
